508ca9a2c10bd3a5b9e68addc4ff37b1b8f0fbe9
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2020-07-11 19:14:54 +0200

    Update Deep Learning 1.py

--- Deep learning/Deep Learning 1.py
+++ Deep learning/Deep Learning 1.py
@@ -32,3 +32,10 @@ model.save('epic_num_reader.model')
 new_model = tf.keras.models.load_model('epic_num_reader.model')
 predictions = new_model.predict([x_test])
 print(predictions)
+
+import numpy as np
+
+print(np.argmax(predictions[0]))
+
+plt.imshow(x_test[0])
+plt.show()
