6d2d086c8426010d3ab44d3351a2ec9bf7abe5f4
Amaury Semery <amaury.semery@laposte.net>
2020-07-15 11:12:36 +0200

    Update Deep Learning 1.py

--- Deep learning/Deep Learning 1.py
+++ Deep learning/Deep Learning 1.py
@@ -4,7 +4,7 @@ mnist = tf.keras.datasets.mnist # 28x28 images of hand-written digits 0-9
 
 (x_train, y_train), (x_text, y_test) = mnist.load_data()
 
-x_train = tf.keras.utils.normalize(x_trains, axis=1)
+x_train = tf.keras.utils.normalize(x_train, axis=1)
 x_test = tf.keras.utils.normalize(x_train, axis=1)
 
 model = tf.keras.models.Sequential()
