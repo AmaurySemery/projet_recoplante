2ae5f90777cb1b65c48b6f30e0056888be5b8689
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2020-07-26 18:33:39 +0200

    Create Prototype.py

---
+++ Deep learning/Prototype.py
@@ -0,0 +1,3 @@
+# Je commence avec TensorFlow Lite pour faire de la reconnaissance en Deep Learning sur mobile
+# Le modèle pré-entraîné que je veux utiliser correspond à "Image classification"
+# Sert à identifier des centaines de classes d'objets, notamment des personnes, des animaux, des lieux mais surtout des plantes (ce qui va nous intéresser)
