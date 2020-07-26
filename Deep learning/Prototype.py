31163ac32660d864ea6c1b6771767d7ebdb4c91b
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2020-07-26 18:44:10 +0200

    Update Prototype.py

--- Deep learning/Prototype.py
+++ Deep learning/Prototype.py
@@ -9,3 +9,41 @@ assert tf.__version__.startswith('2')
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+
+# Je télécharge la base de données sur les fleurs
+
+_URL = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
+
+zip_file = tf.keras.utils.get_file(origin=_URL,
+                                   fname="flower_photos.tgz",
+                                   extract=True)
+
+base_dir = os.path.join(os.path.dirname(zip_file), 'flower_photos')
+
+# J'utilise ImageDataGenerator pour redimensionner les images
+# Ce faisant, il me faut créer le générateur d'entraînement en spécifiant l'emplacement des données au sortir de l'entraînement (les données sur la plante, la taille de l'image, etc.)
+# Ensuite, il me faut créer le générateur de validation avec une approche similaire à la méthode flow_from_directory()
+
+IMAGE_SIZE = 224
+BATCH_SIZE = 64
+
+datagen = tf.keras.preprocessing.image.ImageDataGenerator(
+    rescale=1./255,
+    validation_split=0.2)
+
+train_generator = datagen.flow_from_directory(
+    base_dir,
+    target_size=(IMAGE_SIZE, IMAGE_SIZE),
+    batch_size=BATCH_SIZE,
+    subset='training')
+
+val_generator = datagen.flow_from_directory(
+    base_dir,
+    target_size=(IMAGE_SIZE, IMAGE_SIZE),
+    batch_size=BATCH_SIZE,
+    subset='validation')
+
+for image_batch, label_batch in train_generator:
+  break
+image_batch.shape, label_batch.shape
+
