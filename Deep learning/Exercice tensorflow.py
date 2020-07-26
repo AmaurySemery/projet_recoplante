7141aff98bd4094919a63a08d129b5715a94e7f8
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2020-07-26 17:11:27 +0200

    Update Exercice tensorflow.py

--- Deep learning/Exercice tensorflow.py
+++ Deep learning/Exercice tensorflow.py
@@ -4,3 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 print(tf.__version__)
+
+fashion_mnist = keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
