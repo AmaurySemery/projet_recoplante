8fa63307ff419814d7909960c0df7d611ae87345
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2020-07-26 17:29:23 +0200

    Update Exercice tensorflow.py

--- Deep learning/Exercice tensorflow.py
+++ Deep learning/Exercice tensorflow.py
@@ -1,10 +1,22 @@
-import tensorflow as tf
-from tensorflow import keras
 import numpy as np
-import matplotlib.pyplot as plt
+import tensorflow as tf
 
-print(tf.__version__)
+# Load the TFLite model and allocate tensors.
+interpreter = tf.lite.Interpreter(model_path="converted_model.tflite")
+interpreter.allocate_tensors()
 
-fashion_mnist = keras.datasets.fashion_mnist
+# Get input and output tensors.
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
 
-(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+# Test the model on random input data.
+input_shape = input_details[0]['shape']
+input_data = np.array(np.random.random_sample(input_shape), dtype=np.float32)
+interpreter.set_tensor(input_details[0]['index'], input_data)
+
+interpreter.invoke()
+
+# The function `get_tensor()` returns a copy of the tensor data.
+# Use `tensor()` in order to get a pointer to the tensor.
+output_data = interpreter.get_tensor(output_details[0]['index'])
+print(output_data)
