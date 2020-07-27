c6fc1e6789332ff918c313e12c975cd32ec5966a
Amaury Semery <amaury.semery@laposte.net>
2020-07-27 15:59:49 +0200

    Update Classement et segmentation des données visuelles.py

--- Deep learning/Classement et segmentation des données visuelles.py
+++ Deep learning/Classement et segmentation des données visuelles.py
@@ -1,3 +1,5 @@
+# https://openclassrooms.com/fr/courses/4470531-classez-et-segmentez-des-donnees-visuelles/5024566-appliquez-vos-premiers-traitements-dimages
+
 import matplotlib.pyplot as plt
 
 import numpy as np
@@ -19,3 +21,7 @@ img = np.array(Image.open('/home/popschool/Documents/GitHub/projet_recoplante/Im
 n, bins, patches = plt.hist(img.flatten(), bins=range(256))
 
 plt.show()
+
+# à gauche => pixels noirs ; au milieu => nuances de gris ; à droite => pixels blancs
+
+# Voir la documentation : https://pillow.readthedocs.io/en/3.1.x/reference/Image.html pour utiliser la librairie
