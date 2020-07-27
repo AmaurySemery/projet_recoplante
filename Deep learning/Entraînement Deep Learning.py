ff2e6befb803f5a44d15c8a568cc55035f6786d8
Amaury Semery <amaury.semery@laposte.net>
2020-07-27 15:24:08 +0200

    Create Entraînement Deep Learning.py

---
+++ Deep learning/Entraînement Deep Learning.py
@@ -0,0 +1,9 @@
+from PIL import Image
+
+# Charger l'image
+
+img = Image.open('/home/popschool/Documents/GitHub/projet_recoplante/Images_test/bruyere_des_marais_NB.jpg')
+
+# Afficher l'image chargée
+
+img.show()
